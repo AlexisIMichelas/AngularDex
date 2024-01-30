2617a05f83e967f51768abbc1eb438f35f36cb30
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-01-30 22:58:38 +0100

    update

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core';
 import { PokemonService, Pokemon } from '../pokemon.service';
 
 import { CommonModule } from '@angular/common';
