317e9fc30dbbfa0c7a6670f131467bae7ba26b2b
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-01-30 22:19:12 +0100

    work is ok, need modif

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-details/pokemon-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+export const routes: Routes = [
+
+  { path: '', component: PokemonListComponent },
+
+
+  { path: 'pokemon/:id', component: PokemonDetailComponent, pathMatch: 'full' },
+];
+
